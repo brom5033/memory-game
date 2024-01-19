0a20f791e5e293ad5ea870e3e796d9e1aafb1ad4
brom5033 <sjajdcjddlsl@naver.com>
2024-01-19 22:33:49 +0900

    docs(package.json): add husky , fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,9 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
+        "import/prefer-default-export": "off",
+        "arrow-body-style": "off",
+        "import/no-extraneous-dependencies": ["error", {"devDependencies": ["vite.config.ts"]}],
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
