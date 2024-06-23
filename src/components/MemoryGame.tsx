060c5d60e6caf0da9615f5591b982fca950951be
brom5033 <sjajdcjddlsl@naver.com>
2024-06-23 21:28:48 +0900

    fix(memorygame): remove react import

--- src/components/MemoryGame.tsx
+++ src/components/MemoryGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, FC } from 'react';
+import { useState, useEffect, useRef, FC } from 'react';
 import styled from '@emotion/styled';
 import GameBoard from './GameBoard.tsx';
 
